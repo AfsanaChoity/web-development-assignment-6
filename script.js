ca4417f9c31d5a023beb4e482f9fa14a799324da
choity <afsanamimi194@gmail.com>
2024-03-06 16:43:47 +0600

    loading spinner added

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const displayLatestPost = data => {
         latestPostCard.innerHTML = `
         <div class=" p-6">
         <figure><img src="${eachData.cover_image
-        }" class="rounded-2xl"  alt="Shoes" /></figure>
+            }" class="rounded-2xl"  alt="Shoes" /></figure>
     </div>
     <div class="card-body">
         <div class="flex items-center gap-2">
@@ -22,24 +22,24 @@ const displayLatestPost = data => {
             <p class="text-gray-500 font-normal text-xs mulish-font " id="date">${eachData.author.posted_date}</p>
         </div>
       <h2 class="card-title mulish-font text-xl font-extrabold">${eachData.title
-      }</h2>
+            }</h2>
       <p class="text-gray-500 font-normal text-xs mulish-font ">${eachData.description} </p>
       <div class="flex items-center gap-3">
         <img src="${eachData.
-            profile_image}" alt="profile-picture" class="w-11 h-11 rounded-full">
+                profile_image}" alt="profile-picture" class="w-11 h-11 rounded-full">
         <div >
             <h3 class="mulish-font font-bold text-xs">${eachData.author.name}</h3>
             <p id="designation" class="text-gray-500 font-normal text-xs mulish-font ">${eachData.author.designation}</p>
         </div>
       </div>
     </div>`
-    const noDate = latestPostCard.querySelector("#date");
+        const noDate = latestPostCard.querySelector("#date");
         if (!eachData.author.posted_date) {
             noDate.innerHTML = `<p class="text-gray-500 font-normal text-xs mulish-font">No Publish Date</p>`;
         }
 
         const noDesignation = latestPostCard.querySelector('#designation');
-        if(!eachData.author.designation){
+        if (!eachData.author.designation) {
             noDesignation.innerHTML = `<p class="text-gray-500 font-normal text-xs mulish-font">Unknown</p>`;
         }
         latestPostContainer.appendChild(latestPostCard);
@@ -48,3 +48,88 @@ const displayLatestPost = data => {
 }
 
 latestPost();
+
+const allPost = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const post = await res.json();
+    const posts = post.posts;
+    console.log(posts);
+    if (posts.length > 0){
+        // console.log("1 er besi");
+        document.getElementById("loading").style.display = "none";
+    }
+    displayAllPost(posts);
+
+}
+allPost("");
+
+
+const displayAllPost = posts => {
+    // console.log(posts);
+
+    const postContainer = document.getElementById("post-container");
+    postContainer.textContent= '';
+    posts.forEach(post => {
+        console.log(post);
+
+        const postCard = document.createElement('div');
+        postCard.classList = `card  bg-base-100 bg-gray-200  mb-5 h-60 lg:56 `;
+        postCard.innerHTML = `
+    <div class="card-body">
+    <div class="flex space-x-5">
+      <div class="relative">
+        <img src="${post.image}" alt="profile-picture" class="w-20 h-16 rounded-2xl">
+        <div id="active"  class="absolute top-0 right-0 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></div>
+      </div>
+    <div>
+      <div class="flex text-[#12132D] text-xs font-medium mb-2">
+        <p>#<span>${post.category
+            }</span></p>
+        <p>Author : ${post.author.name}</p>
+      </div>
+
+      <h2 class="card-title text-xl font-bold text-[#12132D] mb-2">${post.title}</h2>
+      <p class="inter-font text-xs font-normal text-gray-400">${post.
+                description}</p>
+     <hr class="my-3 border-1 border-gray-500">
+      <div class="flex items-center">
+        <i class="fa-regular fa-message"></i>
+        <p>${post.
+                comment_count}</p>
+        <i class="fa-regular fa-eye"></i>
+        <p>${post.view_count}</p>
+        <i class="fa-regular fa-clock"></i>
+        <p>${post.
+                posted_time} min</p>
+        <div class="card-actions justify-end ">
+          <button class="btn btn-primary bg-[#10B981] border-none rounded-full"><i class="fa-solid fa-envelope-open"></i></button>
+        </div>
+      </div>
+      
+    </div>
+    </div>
+    
+  
+   
+  </div>
+    `;
+
+        const notActive = postCard.querySelector("#active");
+        if (!post.isActive)
+        {
+            notActive.style.backgroundColor = "red";
+        }
+            postContainer.appendChild(postCard);
+    });
+
+
+}
+
+const handleSearch = () => {
+    const searchField = document.getElementById('search-btn').value;
+    
+    
+    console.log(searchField);
+    allPost(searchField);
+};
+
